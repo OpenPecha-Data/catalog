b0be18f49766150108e688a9d9e68bfa1516cdbd
tenzin <ten13zin@gmail.com>
2020-02-05 12:43:19 +0530

    sort the catalog by ID

--- src/update_catalog.py
+++ src/update_catalog.py
@@ -7,16 +7,20 @@ def add_id_url(row):
     row[0] = f'[{id}](https://github.com/OpenPecha/{id})'
     return row
 
-
 def update_catalog():
     updated_catalog = []
+    # add url to ID
     with open(Catalog_fn) as f:
         csv_reader = csv.reader(f)
         for row in csv_reader:
             if row[0][0] == 'P':
                 updated_catalog.append(add_id_url(row))
-            updated_catalog.append(row)
+            else:
+                updated_catalog.append(row)
 
+    # sort the catalog by ID
+    updated_catalog = sorted(updated_catalog, key=lambda row: row[0])
+    
     with open(Catalog_fn, 'w') as f:
         csv_writer = csv.writer(f)
         csv_writer.writerows(updated_catalog)
